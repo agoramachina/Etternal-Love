5ceac22793e61de37fee90176852a87c04b55882
Barinade <poco0317@users.noreply.github.com>
2021-05-26 20:07:20 -0500

    PU TTHE BANNER ON EVAL DONT FORGET

--- BGAnimations/ScreenEvaluation underlay.lua
+++ BGAnimations/ScreenEvaluation underlay.lua
@@ -11,4 +11,19 @@ if enabled then
 	}
 end
 
+t[#t + 1] =
+	Def.Sprite {
+	Name = "Banner",
+	OnCommand = function(self)
+		self:x(SCREEN_CENTER_X):y(38):valign(0)
+		self:scaletoclipped(capWideScale(get43size(336), 336), capWideScale(get43size(105), 105))
+		local bnpath = GAMESTATE:GetCurrentSong():GetBannerPath()
+		if not bnpath then
+			bnpath = THEME:GetPathG("Common", "fallback banner")
+		end
+		self:LoadBackground(bnpath)
+	end
+}
+
+
 return t
