662e1aa6a1e19d096e572927e2c1619a5daede59
Barinade <poco0317@users.noreply.github.com>
2021-05-26 19:03:34 -0500

    branch lua error

--- Scripts/02 Branches.lua
+++ Scripts/02 Branches.lua
@@ -53,11 +53,7 @@ Branch = {
 		end
 	end,
 	AfterTitleMenu = function()
-		if PREFSMAN:GetPreference("ShowCaution") then
-			return "ScreenCaution"
-		else
-			return Branch.StartGame()
-		end
+		return Branch.StartGame()
 	end,
 	StartGame = function()
 		-- Check to see if there are 0 songs installed. Also make sure to check
